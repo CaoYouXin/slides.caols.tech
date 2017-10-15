ebeaefb789f54bcbb58e988f17dac84f40ba7aa7
曹又心 <c_lisheng@163.com>
2017-10-15 18:04:59 +0800

    build

---
+++ b.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cd webapp \
+    && npm run build \
+    && cd .. \
+    && rm -rf ./docs/ \
+    && git checkout -- docs/CNAME \
+    && cp -r ./webapp/build/ ./docs/ \
+    && cp ./docs/index.html ./docs/404.html
+#    && git add --all \
+#    && git commit -m "$1" \
+#    && git push origin master
