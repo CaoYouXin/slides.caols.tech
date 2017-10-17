cce7671cd1c1de1f6c73cc2f9ed2841485dc546f
曹又心 <c_lisheng@163.com>
2017-10-17 17:37:24 +0800

    hello world slide

--- webapp/src/ppt1/slide1/index.js
+++ webapp/src/ppt1/slide1/index.js
@@ -2,16 +2,40 @@ import React, { Component } from 'react';
 import './index.css';
 
 class PPT1Slide1Component extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: 'Hello World.'
+    };
+  }
+
   componentDidUpdate(prevProps) {
     const { no: prevNo } = prevProps;
     const { no, over } = this.props;
-    if (prevNo !== no && no > 0) {
-      over();
+    if (prevNo !== no) {
+      if (no > 2) {
+        over();
+        return;
+      }
+
+      switch (no) {
+        case 0:
+          this.setState({ text: 'Hello World.' });
+          break;
+        case 1:
+          this.setState({ text: 'Hi Everyone!' });
+          break;
+        case 2:
+          this.setState({ text: "I'm here to represent some things." });
+          break;
+        default:
+          this.setState({ text: 'Hello World.' });
+      }
     }
   }
 
   render() {
-    return (<div className="ppt1-slide1-wrapper">Hello World 1</div>)
+    return (<div className="ppt1-slide1-wrapper mid-box">{this.state.text}</div>);
   }
 }
 
