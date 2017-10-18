79260fc23537f7660504788e809d4025a52b061b
曹又心 <c_lisheng@163.com>
2017-10-18 10:10:50 +0800

    hello world for animation

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -5,6 +5,13 @@ import { Provider } from 'react-redux';
 import { configStore } from './store';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import TWEEN from '@tweenjs/tween.js';
+
+const loop = () => {
+  TWEEN.update();
+  requestAnimationFrame(loop);
+};
+requestAnimationFrame(loop);
 
 ReactDOM.render(<Provider store={configStore()}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
--- webapp/src/ppt1/slide1/index.js
+++ webapp/src/ppt1/slide1/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './index.css';
+import TWEEN from '@tweenjs/tween.js';
 
 class PPT1Slide1Component extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: 'Hello World.'
+      text: 'Hello World.',
+      opacity: 1
     };
   }
 
@@ -18,16 +20,39 @@ class PPT1Slide1Component extends Component {
         return;
       }
 
+      const self = this;
       switch (no) {
-        case 0:
-          this.setState({ text: 'Hello World.' });
-          break;
         case 1:
-          this.setState({ text: 'Hi Everyone!' });
+          var state1 = { x: 0 };
+          var target1 = { x: 1 };
+          var flipped1 = false;
+          var tween1 = new TWEEN.Tween(state1).to(target1, 2000);
+          tween1.onUpdate(function () {
+            if (!flipped1 && state1.x > 0.5) {
+              self.setState({ text: '大家好!' });
+              flipped1 = true;
+            }
+            self.setState({ opacity: Math.abs(state1.x * 2 - 1) });
+          });
+          tween1.easing(TWEEN.Easing.Linear.None);
+          tween1.start();
           break;
         case 2:
-          this.setState({ text: "I'm here to represent some things." });
+          var state2 = { x: 0 };
+          var target2 = { x: 1 };
+          var flipped2 = false;
+          var tween2 = new TWEEN.Tween(state2).to(target2, 2000);
+          tween2.onUpdate(function () {
+            if (!flipped2 && state2.x > 0.5) {
+              self.setState({ text: "我来演示一些东西." });
+              flipped2 = true;
+            }
+            self.setState({ opacity: Math.abs(state2.x * 2 - 1) });
+          });
+          tween2.easing(TWEEN.Easing.Linear.None);
+          tween2.start();
           break;
+        case 0:
         default:
           this.setState({ text: 'Hello World.' });
       }
@@ -35,7 +60,7 @@ class PPT1Slide1Component extends Component {
   }
 
   render() {
-    return (<div className="ppt1-slide1-wrapper mid-box">{this.state.text}</div>);
+    return (<div className="ppt1-slide1-wrapper mid-box"><span style={{ opacity: this.state.opacity }}>{this.state.text}</span></div>);
   }
 }
 
